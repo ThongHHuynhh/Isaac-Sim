899037ba06ce42df3d699a253e78cd1f5efebfe0
Thong Huynh <thong.huynh@abiltd.com>
2026-06-12 10:54:39 -0400

    Add ABB IRB 1200 files as regular directory

---
+++ ABB-IRB-1200/README.md
@@ -0,0 +1,9 @@
+# ABB-IRB-1200
+
+Tutorial on ABB Move-it
+ 
+ ```
+ rosrun xacro xacro --inorder irb1200_5_90.xacro > irb1200.urdf
+ check_urdf <urdf_file>
+ roslaunch moveit_setup_assistant setup_assistant.launch
+ ```
---
+++ python-packages/K2-CV-STEP-001.py
@@ -0,0 +1,23 @@
+from pxr import UsdPhysics, PhysxSchema, Gf
+import omni.usd
+
+stage = omni.usd.get_context().get_stage()
+
+belt_path = "/World/A_900023_INT_00_16_JUL_25_1/tn__A900023INT0016JUL251_lT8CFIMq0/Plane"
+belt_prim = stage.GetPrimAtPath(belt_path)
+
+if not belt_prim.IsValid():
+    raise Exception(f"Prim not found: {belt_path}")
+
+UsdPhysics.CollisionAPI.Apply(belt_prim)
+
+mesh_collision = UsdPhysics.MeshCollisionAPI.Apply(belt_prim)
+mesh_collision.CreateApproximationAttr("convexHull")
+
+rigid_body = UsdPhysics.RigidBodyAPI.Apply(belt_prim)
+rigid_body.CreateKinematicEnabledAttr(True)
+
+surface_velocity = PhysxSchema.PhysxSurfaceVelocityAPI.Apply(belt_prim)
+surface_velocity.CreateSurfaceVelocityAttr(Gf.Vec3f(0.5, 0.0, 0.0))  # +X, 0.5 m/s
+
+print("Applied surface velocity to:", belt_path)
